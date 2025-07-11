a20f7741b338d7e5f82d83b4694891428a083784
Jan Derick Velez <jderickvelez@gmail.com>
2025-07-11 17:07:52 +0800

    fixed json format and file reader

--- main.py
+++ main.py
@@ -11,13 +11,11 @@ office = input("Enter Office:" )
 supplier_request_path = input("Enter Supplier Request: ")
 supplier_response_path =  input("Enter Supplier Response: ")
 
-request = ET.parse(rf"{supplier_request_path}")
-request_root = request.getroot()
-request_text = request_root.text.strip()
-
-response = ET.parse(rf"{supplier_request_path}")
-response_root = response.getroot()
-response_text = response_root.text.strip()
+with open(supplier_request_path, mode='r', encoding='utf-8' ) as file:
+    request_text = file.read()
+    
+with open(supplier_response_path, mode='r', encoding='utf-8' ) as file:
+    response_text = file.read()
 
 gzip_supplier_request = gzip.compress(request_text.encode('utf-8'))
 gzip_supplier_response = gzip.compress(response_text.encode('utf-8'))
@@ -25,14 +23,12 @@ gzip_supplier_response = gzip.compress(response_text.encode('utf-8'))
 compressed_supplier_request = base64.b64encode(gzip_supplier_request)
 compressed_supplier_response = base64.b64encode(gzip_supplier_response)
 
-flight_list = [f"flightQueryId:{flightQueryId}",
-               f"supplier: {supplier}",
-               f"office: {office}",
-               f"supplierRequest: {compressed_supplier_request.decode('utf-8')}",
-               f"supplierResponse: {compressed_supplier_response.decode('utf-8')}"
-               ]
-
-json_obj = json.dumps(flight_list)
+json_obj = json.dumps({"flightQueryId": flightQueryId,
+               "supplier": supplier,
+               "office": office,
+               "supplierRequest": compressed_supplier_request.decode('utf-8'),
+               "supplierResponse": compressed_supplier_response.decode('utf-8')
+               })
 
 currentpath  = os.getcwd()
 filepath = os.path.join("jsonfiles",f'{flightQueryId}-{supplier}.json')
